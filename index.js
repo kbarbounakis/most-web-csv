0d93854959d1fe5438ba1556e4cdc3ce5ebd33a6
Kyriakos Barbounakis <k.barbounakis@gmail.com>
2015-09-14 10:33:10 +0300

    most web csv handler initial commit

---
+++ index.js
@@ -0,0 +1,58 @@
+/**
+ * MOST Web Framework
+ * A JavaScript Web Framework
+ * http://themost.io
+ *
+ * Copyright (c) 2014, Kyriakos Barbounakis k.barbounakis@gmail.com, Anthi Oikonomou anthioikonomou@gmail.com
+ *
+ * Released under the BSD3-Clause license
+ * Date: 2015-09-14
+ */
+var util = require('util');
+
+function HttpCsvResult(data)
+{
+    this.data = data;
+    this.contentType = 'application/csv;charset=utf-8';
+    this.contentEncoding = 'utf8';
+}
+
+HttpCsvResult.prototype.execute = function(context, callback) {
+    var self = this,
+        csv = require('fast-csv');
+    try {
+        context.response.writeHead(this.responseStatus || 200, { "Content-Type": this.contentType });
+        csv.writeToString(this.data, { headers: true, transform: function(row){
+            var keys = Object.keys(row);
+            for (var i = 0; i < keys.length; i++) {
+                var key = keys[i];
+                if (typeof row[key] === 'object' && row[key] != null && !(row[key] instanceof Date)) {
+                    if (row[key].name) {
+                        row[key] = row[key].name;
+                    }
+                }
+            }
+            return row;
+        }}, function(err, data) {
+            if (err) {
+                callback.call(context, err);
+            }
+            else {
+                context.response.write(data, self.contentEncoding);
+                callback.call(context);
+            }
+        });
+    }
+    catch(e) {
+        callback.call(context);
+    }
+};
+
+if (typeof exports !== 'undefined') {
+    module.exports = {
+        /**
+         * @constructs HttpCsvResult
+         */
+        HttpCsvResult:HttpCsvResult
+    };
+}
